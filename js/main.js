0d47c1e924cb516d9850e0e630e807fd388ec7d0
ThomasRinke82 <60111197+ThomasRinke82@users.noreply.github.com>
2020-08-18 16:33:31 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,6 +3,27 @@ class Game {
         this._player1 = player1;
         this._player2 = player2;
     }
+    
+    winDuell(playerOneMove, playerTwoMove) {
+        switch (playerOneMove + playerTwoMove) {
+          case "rockscissors":
+          case "scissorspaper":
+          case "paperrock":
+            //winPlayerOne();
+            console.log('player 1');
+            break;
+          case "rockpaper":
+          case "scissorsrock":
+          case "paperscissors":
+            //winPlayerTwo();
+            console.log('player 2');
+            break;
+          default:
+            console.log('draw');
+            //draw();
+        }
+      }
+    // incremen
 }
 
 class Player {

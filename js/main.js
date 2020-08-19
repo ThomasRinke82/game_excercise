52a37dc699aa2732ee166e8b6f84fb553fc4f6cf
Thomas Rinke <thomasrinke@gmx.de>
2020-08-19 18:41:34 +0200

    added change name buttons

--- js/main.js
+++ js/main.js
@@ -1,89 +1,110 @@
 class Game {
-    constructor(rounds, playerOne, playerTwo) {
-        this._rounds = rounds;
-        this._playerOne = playerOne;
-        this._playerTwo = playerTwo;
+  constructor(rounds, playerOne, playerTwo) {
+    this._rounds = rounds;
+    this._playerOne = playerOne;
+    this._playerTwo = playerTwo;
+  }
+
+  play(playerOne, playerTwo) {
+    console.log(playerOne.move);
+    switch (playerOne.move + playerTwo.move) {
+      case "rockscissors":
+      case "scissorspaper":
+      case "paperrock":
+        playerOne.win();
+        break;
+      case "rockpaper":
+      case "scissorsrock":
+      case "paperscissors":
+        playerTwo.win();
+        break;
+      default:
+        break;
     }
-    
-    play(playerOne, playerTwo) {
-        console.log(playerOne.move);
-        switch (playerOne.move + playerTwo.move) {
-          case "rockscissors":
-          case "scissorspaper":
-          case "paperrock":
-            playerOne.win();
-            break;
-          case "rockpaper":
-          case "scissorsrock":
-          case "paperscissors":
-            playerTwo.win();
-            break;
-          default:
-            break;
-        }
-      }
+  }
 
-      get playerOne() {
-        return this._playerOne;
-      }
+  get playerOne() {
+    return this._playerOne;
+  }
 
-      get playerTwo() {
-        return this._playerTwo;
-      }
+  get playerTwo() {
+    return this._playerTwo;
+  }
 
-      get rounds() {
-        return this._rounds;
-      }
-
+  get rounds() {
+    return this._rounds;
+  }
 }
 
 class Player {
-    constructor(name) {
-        this._name = name;
-        this._gamesWon = 0;
-        this._move = "";
-    }
+  constructor(name) {
+    this._name = name;
+    this._gamesWon = 0;
+    this._move = "";
+  }
 
-    get name() {
-        return this._name;
-    }
+  get name() {
+    return this._name;
+  }
 
-    get gamesWon() {
-        return this._gamesWon;
-    }
+  get gamesWon() {
+    return this._gamesWon;
+  }
 
-    get move() {
-        return this._move;
-    }
+  get move() {
+    return this._move;
+  }
 
-    selectSign(input) {
-        this._move = input;
-    }
+  selectSign(input) {
+    this._move = input;
+  }
 
-    win() {
-        this._gamesWon += 1;
-    }
+  win() {
+    this._gamesWon += 1;
+  }
 }
 
 class Move {
-    constructor(sign) {
-        this._sign = sign;
-    }
+  constructor(sign) {
+    this._sign = sign;
+  }
 
-    get sign() {
-        return this._sign;
-    }
+  get sign() {
+    return this._sign;
+  }
 }
-
 
 // Initial set-up - before start screen
 let scissors = new Move("scissors");
 let rock = new Move("rock");
 let paper = new Move("paper");
 
+//function to ask the player to insert name
+//via prompt and then display the value as playerName
+
+//player1
+const btnNameOne = document.getElementById("player1-name");
+const enterNameOne = document.getElementById("player1");
+
+btnNameOne.addEventListener("click", function () {
+  let playerName = prompt("Enter your name here:");
+  enterNameOne.innerHTML = playerName;
+  btnNameOne.style.display = "none";
+});
+
+//player2 -> find a way to reduce duplicate code
+const btnNameTwo = document.getElementById("player2-name");
+const enterNameTwo = document.getElementById("player2");
+
+btnNameTwo.addEventListener("click", function () {
+  let playerName = prompt("Enter your name here:");
+  enterNameTwo.innerHTML = playerName;
+  btnNameTwo.style.display = "none";
+});
+
 // Player Creation - start screen
 let createFirst = new Player("Kevin");
-let createSecond = new  Player("Aria");
+let createSecond = new Player("Aria");
 
 // Game Initialization
 let newGame = new Game(3, createFirst, createSecond);
@@ -94,16 +115,19 @@ let playerTwo = newGame.playerTwo;
 
 //Game Loop
 while (true) {
-    if (playerOne.gamesWon === newGame.rounds || playerTwo.gamesWon === newGame.rounds) {
-        console.log("Player won!");
-        break;
-    } else {
-        // Player input has to be implemented - DOM
-        playerOne.selectSign(scissors.sign);
-        playerTwo.selectSign(rock.sign);
-        newGame.play(playerOne, playerTwo);
-        console.log(`${playerOne.name}: ${playerOne.gamesWon} - ${playerTwo.name}: ${playerTwo.gamesWon}`);
-    }
-    }
-
-
+  if (
+    playerOne.gamesWon === newGame.rounds ||
+    playerTwo.gamesWon === newGame.rounds
+  ) {
+    console.log("Player won!");
+    break;
+  } else {
+    // Player input has to be implemented - DOM
+    playerOne.selectSign(scissors.sign);
+    playerTwo.selectSign(rock.sign);
+    newGame.play(playerOne, playerTwo);
+    console.log(
+      `${playerOne.name}: ${playerOne.gamesWon} - ${playerTwo.name}: ${playerTwo.gamesWon}`
+    );
+  }
+}
